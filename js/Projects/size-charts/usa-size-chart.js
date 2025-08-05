b9a0059c616f2394bfba25160d295c7081af97eb
Tima <abri.timka@gmail.com>
2025-08-06 01:53:56 +0300

    AddSizeChart

---
+++ js/Projects/size-charts/usa-size-chart.js
@@ -0,0 +1,23 @@
+export const chestIntervalUsa = [
+  { min: 32.3, max: 33.9, size: "S" },
+  { min: 45.5, max: 35.4, size: "M" },
+  { min: 89.6, max: 67.5, size: "L" },
+  { min: 87.4, max: 47.5, size: "XL" },
+  { min: 90.5, max: 45.3, size: "XXL" },
+];
+
+export const hipIntervalUsa = [
+  { min: 12.3, max: 14.4, size: "S" },
+  { min: 23.4, max: 24.5, size: "M" },
+  { min: 84.5, max: 99.5, size: "L" },
+  { min: 34.5, max: 95.3, size: "XL" },
+  { min: 12.4, max: 56.5, size: "XXL" },
+];
+
+export const stretchIntervalUsa = [
+  { size: "S", stretch: "Non" },
+  { size: "M", stretch: "Medium" },
+  { size: "L", stretch: "High" },
+  { size: "XL", stretch: "Slight" },
+  { size: "XXL", stretch: "Medium" },
+];
